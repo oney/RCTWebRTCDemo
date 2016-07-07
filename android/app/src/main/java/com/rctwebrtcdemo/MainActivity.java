d303ba286a509209f80a552ec59f87986065d496
oney <appdevoney@gmail.com>
2016-07-07 18:31:19 +0800

    upgrade to RN 0.29.0

--- android/app/src/main/java/com/rctwebrtcdemo/MainActivity.java
+++ android/app/src/main/java/com/rctwebrtcdemo/MainActivity.java
@@ -1,13 +1,6 @@
 package com.rctwebrtcdemo;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-
-import java.util.Arrays;
-import java.util.List;
-
-import com.oney.WebRTCModule.WebRTCModulePackage;
 
 public class MainActivity extends ReactActivity {
 
@@ -18,26 +11,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "RCTWebRTCDemo";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new WebRTCModulePackage()
-        );
     }
 }
